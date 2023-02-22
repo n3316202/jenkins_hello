be3f541dbfe65a880f10e322517e454c339eb908
chlee807 <116716056+chlee807@users.noreply.github.com>
2023-02-22 14:03:44 +0900

    Add files via upload
    jenkinstestproject

---
+++ src/main/java/edu/global/ex/HomeController.java
@@ -0,0 +1,13 @@
+package edu.global.ex;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping
+	public String home() {
+		return "hello jenkins!!";
+	}
+}
---
+++ src/main/java/edu/global/ex/SpringBootLchJenkinsHelloApplication.java
@@ -0,0 +1,13 @@
+package edu.global.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLchJenkinsHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLchJenkinsHelloApplication.class, args);
+	}
+
+}
